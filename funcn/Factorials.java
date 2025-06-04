65d0b9602439666f250bd72cd8df98456be6a4cd
Nimish182 <nbsutar101104@gmail.com>
2025-06-04 22:42:18 +0530

    adding negative nos factorial condition in the code

--- funcn/Factorials.java
+++ funcn/Factorials.java
@@ -4,6 +4,10 @@ class Factorials {
     public static void printFactorial(int n) {
         int fact = 1; // intialiazing with 1 as zero cannot be multiplied and factorial of 0 is also 1
         // loop
+        if (n < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+            return; // return statement to exit the function
+        }
         for (int i = n; i >= 1; i--) {
             fact = fact * i; // multiplying the number with the factorial
         }
