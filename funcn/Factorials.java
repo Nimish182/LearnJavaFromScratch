68eb266d30eebce1857e9fe7e3fb6aa77e2b8b02
Nimish182 <nbsutar101104@gmail.com>
2025-06-04 22:39:52 +0530

    adding factorial code

---
+++ funcn/Factorials.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Factorials {
+    public static void printFactorial(int n) {
+        int fact = 1; // intialiazing with 1 as zero cannot be multiplied and factorial of 0 is also 1
+        // loop
+        for (int i = n; i >= 1; i--) {
+            fact = fact * i; // multiplying the number with the factorial
+        }
+
+        System.out.println("Factorial of " + n + " is: " + fact);
+        return; // return statement to exit the function
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to calculate its factorial: ");
+        int n = sc.nextInt();
+
+        printFactorial(n);
+    }
+
+}
