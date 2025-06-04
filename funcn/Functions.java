67955618e946648d7c8fe137271aa43b8cd8129d
Nimish182 <nbsutar101104@gmail.com>
2025-06-04 21:27:48 +0530

    Adding code of sum of 2 nos usning function

--- funcn/Functions.java
+++ funcn/Functions.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 
-class Sum {
+class Functions {
     public static int calculateSum(int a, int b) {
         int sum = a + b;
         return sum;
@@ -8,12 +8,13 @@ class Sum {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of a:");
+        // System.out.println("Enter the value of a:");
         int a = sc.nextInt();
-        System.out.println("Enter the value of b:");
+        // System.out.println("Enter the value of b:");
         int b = sc.nextInt();
+
         int sum = calculateSum(a, b); // Calling the function to calculate sum
-        System.out.println(sum);
+        System.out.println("SUm of two numbers is" + sum);
     }
 
 }
